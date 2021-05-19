c9206c18df24e11abbf6e37ddf976b4685292bfe
wtaisner@man.poznan.pl <Mzk5MTY0NTg1OTU0Ovu3YSgVZ+zprEjbjLbqnXY/ABt4>
2021-05-19 09:59:21 +0200

    update karma, remove some redundant packages

--- karma.conf.js
+++ karma.conf.js
@@ -3,53 +3,49 @@ process.env.CHROME_BIN = puppeteer.executablePath();
 
 module.exports = function (config) {
     config.set({
-        basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
-        plugins: [
-            require('karma-jasmine'),
-            require('karma-babel-preprocessor'),
-            require('karma-chrome-launcher'),
-            require('karma-webpack'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma'),
-        ],
-        customLaunchers: {
-            ChromeHeadless: {
-                base: 'Chrome',
-                flags: [
-                    '--headless',
-                    '--disable-gpu',
-                    // Without a remote debugging port, Google Chrome exits immediately.
-                    '--remote-debugging-port=9222',
-                ],
-            }
-        },
-        client:{
-            clearContext: false
-        },
-        files: [
-            
-        ],
-        preprocessors: {},
-        mime: {
-            'text/x-typescript': ['ts','tsx']
-        },
-        coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
-            fixWebpackSourcePaths: true,
-            thresholds: {
-                statements: 30
-            }
-        },
-        
-        reporters: ['progress'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_WARN,
-        autoWatch: false,
-        browsers: ['Chrome', 'ChromeHeadless'],
-        singleRun: false
-    }
+            basePath: '',
+            frameworks: ['jasmine', '@angular-devkit/build-angular'],
+            plugins: [
+                require('karma-jasmine'),
+                require('karma-coverage'),
+                require('karma-chrome-launcher'),
+                require('karma-jasmine-html-reporter'),
+                require('@angular-devkit/build-angular/plugins/karma'),
+            ],
+            customLaunchers: {
+                ChromeHeadless: {
+                    base: 'Chrome',
+                    flags: [
+                        '--headless',
+                        '--disable-gpu',
+                        // Without a remote debugging port, Google Chrome exits immediately.
+                        '--remote-debugging-port=9222',
+                    ],
+                }
+            },
+            client: {
+                clearContext: false
+            },
+            files: [],
+            preprocessors: {},
+            mime: {
+                'text/x-typescript': ['ts', 'tsx']
+            },
+            coverageReporter: {
+                dir: 'coverage',
+                reporters: [
+                    {type: 'html', subdir: '.'},
+                    {type: 'lcovonly', subdir: '.', file: 'lcov.info'},
+                ]
+            },
+
+            reporters: ['progress'],
+            port: 9876,
+            colors: true,
+            logLevel: config.LOG_WARN,
+            autoWatch: false,
+            browsers: ['Chrome', 'ChromeHeadless'],
+            singleRun: false
+        }
     );
 };
