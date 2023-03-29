f781cc9e771506edac938bdfa69e33582124f364
kbeyro <121854496+kbeyro@users.noreply.github.com>
2023-03-29 17:03:59 +0200

    update route

--- src/app/appmarket/appinstance/appinstance.routes.ts
+++ src/app/appmarket/appinstance/appinstance.routes.ts
@@ -2,21 +2,19 @@ import { Route } from '@angular/router';
 import { AppInstanceComponent } from './index';
 import { AppInstanceListComponent } from './index';
 import {AppInstanceShellViewComponent} from './appinstance-shell-view/appinstance-shell-view.component';
-import {AppdeploymentComponent} from '../appdeployment/appchoose/appdeployment.component';
-import {AppnavigatorComponent} from '../appdeployment/appnavigator/appnavigator.component';
-import {AppuploadComponent} from '../appdeployment/appupload/appupload.component';
-import {AppsummaryComponent} from '../appdeployment/appsummary/appsummary.component';
+import {AppdeploymentComponent} from '../bulkDeployment/appDeployment/appchoose/appdeployment.component';
+import {AppnavigatorComponent} from '../bulkDeployment/appDeployment/appnavigator/appnavigator.component';
+import {AppuploadComponent} from '../bulkDeployment/appDeployment/appupload/appupload.component';
+import {AppsummaryComponent} from '../bulkDeployment/appDeployment/appsummary/appsummary.component';
 
 export const AppInstanceRoutes: Route[] = [
     { path: 'instances', component: AppInstanceListComponent },
-    { path: 'instances/:id', component: AppInstanceComponent },
-    { path: 'instances/:id/shell/:podname', component: AppInstanceShellViewComponent },
-    {
-        path: 'deploy', component: AppnavigatorComponent, children: [
+    { path: 'instances/deploy', component: AppnavigatorComponent, children: [
             {path: '', redirectTo: 'select', pathMatch: 'full'},
             {path: 'select', component: AppdeploymentComponent},
             {path: 'upload', component: AppuploadComponent},
             {path: 'summary', component: AppsummaryComponent}
-        ]
-    }
+        ]},
+    { path: 'instances/:id', component: AppInstanceComponent },
+    { path: 'instances/:id/shell/:podname', component: AppInstanceShellViewComponent },
 ];
