57a5c74b2d0761ffd22b40c93af1847f097dc443
kbeyro <121854496+kbeyro@users.noreply.github.com>
2023-08-18 09:30:57 +0200

    Improve roles directive with excluded option

--- src/app/directive/roles.directive.ts
+++ src/app/directive/roles.directive.ts
@@ -1,31 +1,75 @@
 import {AuthService} from '../auth/auth.service';
-import {Directive, TemplateRef, ViewContainerRef, Input} from '@angular/core';
+import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
+
+class RoleState {
+    public allowed: Array<string> = new Array<string>();
+    public excluded: Array<string> = new Array<string>()
+}
+
 
 @Directive({
-  selector: '[roles]',
-  inputs: ['roles']
+    selector: '[roles]',
+    inputs: ['roles']
 })
 export class RolesDirective {
 
-  constructor(private _templateRef: TemplateRef<any>,
-    private _viewContainer: ViewContainerRef,
-    private authService: AuthService) {
+    private _allowed: Array<string> = new Array<string>();
 
-  }
+    private _excluded: Array<string> = new Array<string>();
 
-  @Input() set roles(allowedRoles: Array<string>) {
-    let show: boolean = false;
-    for (let allowedRole of allowedRoles) {
-      if (this.authService.hasRole(allowedRole)) {
-        show = true;
-        break;
-      }
+    constructor(private _templateRef: TemplateRef<any>,
+                private _viewContainer: ViewContainerRef,
+                private authService: AuthService) {
+
     }
 
-    if (show) {
-      this._viewContainer.createEmbeddedView(this._templateRef);
-    } else {
-      this._viewContainer.clear();
+    @Input() set roles(allowedRoles: Array<string>) {
+        this._allowed = allowedRoles;
+        this.updateState({
+            allowed: this._allowed,
+            excluded: this._excluded
+        })
     }
-  }
+
+    @Input() set rolesExcluded(excluded: Array<string>) {
+        this._excluded = excluded;
+        this.updateState({
+            allowed: this._allowed,
+            excluded: this._excluded
+        })
+    }
+
+    updateState(state: RoleState) {
+        this._viewContainer.clear();
+
+        let show: boolean = false;
+        let notAllowed: boolean = false;
+
+        const allowedRoles = state.allowed;
+
+        for (let exclude of state.excluded) {
+            if (this.authService.hasRole(exclude)) {
+                notAllowed = true;
+                break;
+            }
+        }
+        if (notAllowed) {
+            this._viewContainer.clear();
+        } else {
+            for (let allowedRole of allowedRoles) {
+                if (this.authService.hasRole(allowedRole)) {
+                    show = true;
+                    break;
+                }
+            }
+
+            if (show) {
+                this._viewContainer.createEmbeddedView(this._templateRef);
+            } else {
+                this._viewContainer.clear();
+            }
+        }
+
+    }
+
 }
