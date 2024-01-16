882fcebc65687363971349e90bf6ac8e4cec0f9c
pgiertych <pgiertych@man.poznan.pl>
2024-01-16 14:16:13 +0100

    fix test

--- src/app/appmarket/appinstance/app-log-access/app-log-access.component.spec.ts
+++ src/app/appmarket/appinstance/app-log-access/app-log-access.component.spec.ts
@@ -1,23 +1,28 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AppLogAccessComponent} from './app-log-access.component';
+import {AppLogsService} from '../../../service/app-logs.service';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
+import {of} from 'rxjs';
 
 describe('AppLogAccessComponent', () => {
-  let component: AppLogAccessComponent;
-  let fixture: ComponentFixture<AppLogAccessComponent>;
+    let component: AppLogAccessComponent;
+    let fixture: ComponentFixture<AppLogAccessComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AppLogAccessComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AppLogAccessComponent],
+            providers: [AppLogsService, HttpClient, HttpHandler, {provide: ActivatedRoute, useValue: {params: of({id: 1})}}]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(AppLogAccessComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(AppLogAccessComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/service/app-logs-service.service.spec.ts
+++ src/app/service/app-logs-service.service.spec.ts
@@ -1,12 +1,16 @@
 import {TestBed} from '@angular/core/testing';
 
 import {AppLogsService} from './app-logs.service';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {AppConfigService} from './appconfig.service';
 
 describe('AppLogsServiceService', () => {
   let service: AppLogsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AppLogsService, HttpHandler, HttpClient, AppConfigService]
+    });
     service = TestBed.inject(AppLogsService);
   });
 
