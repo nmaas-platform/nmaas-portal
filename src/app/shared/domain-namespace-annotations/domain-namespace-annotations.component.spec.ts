11c9de2c9ac05bd77a4b58798513153cea552ce6
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-02-19 13:54:54 +0100

    update test

--- src/app/shared/domain-namespace-annotations/domain-namespace-annotations.component.spec.ts
+++ src/app/shared/domain-namespace-annotations/domain-namespace-annotations.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DomainNamespaceAnnotationsComponent } from './domain-namespace-annotations.component';
+import {TranslateFakeLoader, TranslateLoader, TranslateModule} from '@ngx-translate/core';
 
 describe('DomainNamespaceAnnotationsComponent', () => {
   let component: DomainNamespaceAnnotationsComponent;
@@ -8,7 +9,15 @@ describe('DomainNamespaceAnnotationsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DomainNamespaceAnnotationsComponent ]
+      declarations: [ DomainNamespaceAnnotationsComponent ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        })
+      ]
     })
     .compileComponents();
 
