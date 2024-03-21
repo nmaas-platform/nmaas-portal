36e5533511b7f97b5179901b3627726338496776
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-03-21 13:11:37 +0100

    update test

--- src/app/appmarket/appmanagement/app-change-owner-modal/app-change-owner-modal.component.spec.ts
+++ src/app/appmarket/appmanagement/app-change-owner-modal/app-change-owner-modal.component.spec.ts
@@ -11,7 +11,7 @@ describe('AppChangeOwnerModalComponent', () => {
     let component: AppChangeOwnerModalComponent;
     let fixture: ComponentFixture<AppChangeOwnerModalComponent>;
 
-    const appsServiceSpy = createSpyObj<AppsService>('AppsServiceSpy', ['updateApplicationBase']);
+    const appsServiceSpy = createSpyObj<AppsService>('AppsServiceSpy', ['updateApplicationBaseOwner']);
     appsServiceSpy.updateApplicationBaseOwner.and.returnValue(of('1'))
 
     const userServiceSpy = createSpyObj<UserService>('UserServiceSpy', ['getAll'])
