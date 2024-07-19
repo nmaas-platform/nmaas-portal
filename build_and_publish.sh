0faa859af3426ceb8534d372a060492e1ec34e9f
Łukasz Łopatowski <lukasz.lopatowski@gmail.com>
2024-07-19 16:12:58 +0200

    Updated version to 1.6.2

--- build_and_publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TAG=1.7.0
-PACKAGE=nmaas-portal
-REPOSITORY=artifactory.geant.net/nmaas-docker-local
-sudo docker build --rm -t $REPOSITORY/$PACKAGE:$TAG -f ./Dockerfile ..
-sudo docker push $REPOSITORY/$PACKAGE:$TAG
