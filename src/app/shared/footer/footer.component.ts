735a35f9e8aed690f520909b23b65e1851a901b5
jkazmierczak <jkazmierczak@man.poznan.pl>
2024-08-19 17:14:47 +0200

    fixed docs links

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -13,15 +13,18 @@ import {TranslateService} from '@ngx-translate/core';
 export class FooterComponent implements OnInit {
 
   public gitInfo: GitInfo;
+  public landingProfile = '';
 
   constructor(private changelogService: ChangelogService, private router: Router,
-              public appConfigService: AppConfigService, public translate: TranslateService) {
+              public appConfigService: AppConfigService, public translate: TranslateService, private appConfig: AppConfigService) {
   }
 
   ngOnInit() {
     if (this.appConfigService.getShowGitInfo()) {
         this.changelogService.getGitInfo().subscribe(info => this.gitInfo = info);
     }
+    this.landingProfile = this.appConfig.getLandingProfile();
+    console.log("Landing profile = ", this.landingProfile)
   }
 
   public moveToTop() {
