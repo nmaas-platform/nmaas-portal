1071d833c71ddc42041ee69f70158bfd75925dde
kbeyro <121854496+kbeyro@users.noreply.github.com>
2024-11-29 09:41:20 +0100

    fix test with logins

--- src/app/auth/login-success/login-success.component.spec.ts
+++ src/app/auth/login-success/login-success.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginSuccessComponent } from './login-success.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { ActivatedRouteStub } from '../../shared/test-utils';
+import { AuthService } from '../auth.service';
 
 describe('LoginSuccessComponent', () => {
   let component: LoginSuccessComponent;
   let fixture: ComponentFixture<LoginSuccessComponent>;
 
+  const authUserSpy = jasmine.createSpyObj('AuthService', ['storeToken']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginSuccessComponent ]
+      declarations: [ LoginSuccessComponent ],
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        {provide: ActivatedRoute, useValue: new ActivatedRouteStub({token: '123'})},
+        {provide: AuthService, useValue: authUserSpy},
+      ]
     })
     .compileComponents();
 
