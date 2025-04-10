2d8508c0a13aa993f9ce964e9a807c5cb17e1703
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-04-10 14:13:56 +0200

    fix problem with displaying error based on structure

--- src/app/appmarket/domains/domain/domain.component.ts
+++ src/app/appmarket/domains/domain/domain.component.ts
@@ -109,7 +109,10 @@ export class DomainComponent extends BaseComponent implements OnInit {
                 this.router.navigate(['admin/domains/'])
         }, err => {
             console.error(err);
-            this.errorMessage = err.message;
+            console.warn("error", err.statusCode)
+            if(err.statusCode !== 409 && err?.message !== undefined) this.errorMessage = err.message;
+            else this.errorMessage = err;
+    
         });
         }
         this.domainService.setUpdateRequiredFlag(true);
