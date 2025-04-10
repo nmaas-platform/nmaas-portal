2f9078e622c40a4a3f76f6a64287690e17fa197e
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-04-10 11:28:18 +0200

    fix test

--- src/app/shared/admin/clusters/manager/manager.component.spec.ts
+++ src/app/shared/admin/clusters/manager/manager.component.spec.ts
@@ -1,23 +1,129 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ClusterManagerComponent } from './manager.component';
+import { ClusterManagerService } from '../../../../service/cluster-manager.service';
+import { of } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('ManagerComponent', () => {
+describe('ClusterManagerComponent', () => {
   let component: ClusterManagerComponent;
   let fixture: ComponentFixture<ClusterManagerComponent>;
+  let clusterService: jasmine.SpyObj<ClusterManagerService>;
+  let mockClusters: any[];
 
   beforeEach(async () => {
+    const clusterServiceSpy = jasmine.createSpyObj('ClusterManagerService', ['getAllClusters', 'sendCluster']);
+
+    mockClusters = [
+      {
+        id: 1,
+        name: 'Cluster A',
+        codename: 'CodeA',
+        creationDate: new Date('2025-01-01'),
+        modificationDate: new Date('2025-02-01'),
+        pathConfigFile: '/path/to/configA.yaml',
+        description: 'Description A',
+        clusterConfigFile: 'ConfigA',
+        ingress: {
+          id: 1,
+          controllerConfigOption: 'OptionA',
+          controllerChartName: 'ChartA',
+          controllerChartArchive: 'ArchiveA',
+          resourceConfigOption: 'ResourceA',
+          externalServiceDomain: 'domainA.com',
+          tlsSupported: true,
+          supportedIngressClass: 'ClassA',
+          certificateConfigOption: 'CertOptionA',
+          issuerOrWildcardName: 'WildcardA',
+          ingressPerDomain: false,
+          publicIngressClass: 'PublicClassA',
+          publicServiceDomain: 'public.domainA.com'
+        },
+        deployment: {
+          smtpServerHostname: 'smtp.domainA.com',
+          smtpServerPort: '587',
+          smtpServerUsername: 'userA',
+          smtpServerPassword: 'passwordA',
+          defaultNamespace: 'namespaceA',
+          defaultStorageClass: 'storageClassA',
+          id: 1,
+          namespaceConfigOption: 'OptionA',
+          forceDedicatedWorkers: true
+        },
+        externalNetworks: [
+          {
+            assigned: true,
+            assignedSince: '2025-01-01',
+            assignedTo: 'UserA',
+            externalIp: '192.168.1.1',
+            externalNetwork: 'NetworkA',
+            externalNetworkMaskLength: 24,
+            id: 1
+          }
+        ]
+      },
+      {
+        id: 2,
+        name: 'Cluster B',
+        codename: 'CodeB',
+        creationDate: new Date('2025-01-15'),
+        modificationDate: new Date('2025-02-15'),
+        pathConfigFile: '/path/to/configB.yaml',
+        description: 'Description B',
+        clusterConfigFile: 'ConfigB',
+        ingress: 'IngressB',
+        deployment: 'DeploymentB',
+        externalNetworks: []
+      }
+    ];
+
+    clusterServiceSpy.getAllClusters.and.returnValue(of(mockClusters));
+
     await TestBed.configureTestingModule({
-      imports: [ClusterManagerComponent]
-    })
-    .compileComponents();
-    
+      declarations: [ClusterManagerComponent],
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: ClusterManagerService, useValue: clusterServiceSpy }
+      ],
+       schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ClusterManagerComponent);
     component = fixture.componentInstance;
+    clusterService = TestBed.inject(ClusterManagerService) as jasmine.SpyObj<ClusterManagerService>;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getAllClusters on initialization', () => {
+    component.getAllClusters();
+    expect(clusterService.getAllClusters).toHaveBeenCalled();
+    expect(component.clusters).toEqual(mockClusters);
+  });
+
+  it('should call sendCluster when a file is uploaded', () => {
+    const mockFile = new File(['test content'], 'test.yaml', { type: 'application/x-yaml' });
+    const mockEvent = { files: [mockFile] };
+    const mockResponse = {
+      id: 3,
+      name: 'Cluster C',
+      codename: 'CodeC',
+      creationDate: new Date(),
+      modificationDate: new Date(),
+      pathConfigFile: '/path/to/configC.yaml',
+      description: 'Description C',
+      clusterConfigFile: 'ConfigC',
+      ingress: null,
+      deployment: null,
+      externalNetworks: []
+    };
+
+    clusterService.sendCluster.and.returnValue(of(mockResponse));
+
+    component.sendCluster(mockEvent);
+    expect(clusterService.sendCluster).toHaveBeenCalledWith(mockFile, jasmine.any(Object));
   });
 });
