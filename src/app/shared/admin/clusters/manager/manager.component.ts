ac5e96b75841f3e00f3750037d1ff5f43a1a7ef7
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-04-16 13:39:53 +0200

    update creation of new connected cluster

--- src/app/shared/admin/clusters/manager/manager.component.ts
+++ src/app/shared/admin/clusters/manager/manager.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ClusterManagerService } from '../../../../service/cluster-manager.service';
 import { ClusterManager } from '../../../../model/cluster-manager';
+import { ModalComponent } from '../../../modal';
 
 @Component({
   selector: 'app-manager',
@@ -11,27 +12,42 @@ export class ClusterManagerComponent {
 
   public clusters: ClusterManager[] = [];
 
+  public addedCluster: ClusterManager = new ClusterManager();
+  public updatedFile : File = null;
+
+    @ViewChild(ModalComponent, { static: true })
+    public modal: ModalComponent;
+
   constructor(private clusterService: ClusterManagerService) {
     this.getAllClusters();
   }
 
  public sendCluster(event: any) {
     console.log(event);
-    const file = event.files[0];
-    const view = new ClusterManager();
-    view.name = "test"
-    view.description="testest"
-    this.clusterService.sendCluster(file, view).subscribe(result => { 
-        console.log(result);
-    }
-    )
+    this.updatedFile =event.files[0];     
     }
 
-    public getAllClusters() {
-        this.clusterService.getAllClusters().subscribe(result => { 
+public getAllClusters() {
+    this.clusterService.getAllClusters().subscribe(result => { 
           console.log(result);
             this.clusters = result;
         })
       }
+
+public closeModalAndSaveCluster() {
+  this.clusterService.sendCluster(this.updatedFile, this.addedCluster).subscribe(result => { 
+    console.log(result);
+    this.getAllClusters();
+    this.modal.hide();
+    this.updatedFile = null;
+    this.addedCluster = new ClusterManager();
+  }, error => {
+    console.error(error);
+    
+  }
+)
+}
+
+
 
 }
