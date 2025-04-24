36fc7289e02965b88a369931fad5dc9721dc4852
pkazimierowski <pkazimierowski@man.poznan.pl>
2025-04-24 09:25:37 +0200

    refresh token improvements

--- src/app/auth/login-success/login-success.component.ts
+++ src/app/auth/login-success/login-success.component.ts
@@ -17,14 +17,18 @@ export class LoginSuccessComponent implements OnInit {
     ngOnInit(): void {
         this.route.queryParams.subscribe(params => {
             const token = params['token'];
-            const refreshToken = params['refresh_token'];
-            const oidcToken = params['oidc_token'];
+            const refreshToken = params['refresh-token'];
+            const oidcToken = params['oidc-token'];
             if (token) {
                 this.authService.storeToken(token);
             }
-            if (refreshToken) {
+            if (oidcToken) {
                 this.authService.storeOidcToken(oidcToken);
             }
+            if (refreshToken) {
+                this.authService.storeRefreshToken(refreshToken);
+            }
+            this.authService.loadUser();
             this.router.navigate(['/'])
         })
 
