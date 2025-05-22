56f8f4af4dc251e903243f033e2f2e37a564c228
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-05-22 14:04:00 +0200

    webhook updead, add cluster selection

--- src/app/service/appinstance.service.ts
+++ src/app/service/appinstance.service.ts
@@ -63,6 +63,11 @@ export class AppInstanceService extends GenericDataService {
         return this.post<AppInstanceRequest, Id>(this.getUrl() + 'domain/' + domainId, new AppInstanceRequest(appId, name, autoUpgradesEnabled));
     }
 
+    public createAppInstanceInCluster(domainId: number, appId: number, name: string, autoUpgradesEnabled: boolean, clusterId: number):  Observable<Id> {
+        const params = new HttpParams().set('clusterId', clusterId.toString());
+        return this.http.post<Id>(this.getUrl() + 'domain/' + domainId, new AppInstanceRequest(appId, name, autoUpgradesEnabled), { params });
+    }
+
     public removeAppInstance(appInstanceId: number): Observable<any> {
         return this.delete<any>(this.getUrl() + appInstanceId);
     }
