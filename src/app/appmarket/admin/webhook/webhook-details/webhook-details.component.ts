c64ab643c94e9190e9d195d166279c7a1570b7d6
kbeyro <121854496+kbeyro@users.noreply.github.com>
2025-05-28 13:38:18 +0200

    add check if value is preseng

--- src/app/appmarket/admin/webhook/webhook-details/webhook-details.component.ts
+++ src/app/appmarket/admin/webhook/webhook-details/webhook-details.component.ts
@@ -69,4 +69,14 @@ export class WebhookDetailsComponent extends BaseComponent implements OnInit {
         this.webhook.authorizationHeader = this.authorizationHeader; 
       }
     }
+
+    public isFormValid(): boolean {
+      if(this.authRequired) {
+               this.webhook.authorizationHeader !== null && this.webhook.authorizationHeader !== "")
+        return this.webhook.tokenValue !== null && this.webhook.tokenValue !== "" &&
+               this.webhook.authorizationHeader !== null && this.webhook.authorizationHeader !== "";
+      } else {
+        return true;
+      } 
+    }
 }
