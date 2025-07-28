b4d2690afb23c3fda5e6648eac3b8eab74ec1f7f
jkazmierczak <jkazmierczak@man.poznan.pl>
2025-07-28 14:07:35 +0200

    sorting in access methods modal

--- src/app/appmarket/appinstance/modals/access-methods-modal/access-methods-modal.component.ts
+++ src/app/appmarket/appinstance/modals/access-methods-modal/access-methods-modal.component.ts
@@ -29,7 +29,16 @@ export class AccessMethodsModalComponent implements OnInit {
     if (this.accessMethods) {
       this.externalAccessMethods = this.accessMethods.filter(s => this.accessMethodTypeAsEnum(s.type) === ServiceAccessMethodType.EXTERNAL
           || this.accessMethodTypeAsEnum(s.type) === ServiceAccessMethodType.DEFAULT
-          || this.accessMethodTypeAsEnum(s.type) === ServiceAccessMethodType.INTERNAL);
+          || this.accessMethodTypeAsEnum(s.type) === ServiceAccessMethodType.INTERNAL).sort((a, b) => {
+            const order = {
+              [ServiceAccessMethodType.INTERNAL]: 0,
+              [ServiceAccessMethodType.EXTERNAL]: 1,
+              [ServiceAccessMethodType.DEFAULT]: 2
+            }
+            const aType = this.accessMethodTypeAsEnum(a.type);
+            const bType = this.accessMethodTypeAsEnum(b.type);
+            return order[aType] - order[bType];
+      });
       this.internalAccessMethods = this.accessMethods.filter(s => this.accessMethodTypeAsEnum(s.type) === ServiceAccessMethodType.INTERNAL);
       this.publicAccessMethods = this.accessMethods.filter(s => this.accessMethodTypeAsEnum(s.type) === ServiceAccessMethodType.PUBLIC);
       this.localAccessMethods = this.accessMethods.filter(s => this.accessMethodTypeAsEnum(s.type) === ServiceAccessMethodType.LOCAL);
