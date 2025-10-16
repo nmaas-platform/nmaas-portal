4b675b9a4cb071d8064bffaa1a15aca942d0162e
pkazimierowski <pkazimierowski@man.poznan.pl>
2025-10-16 09:33:42 +0200

    changed logic about loading domain id

--- src/app/appmarket/applist/applist.component.ts
+++ src/app/appmarket/applist/applist.component.ts
@@ -5,11 +5,12 @@ import {UserDataService} from '../../service/userdata.service';
 import {AppViewType} from '../../shared/common/viewtype';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Location} from '@angular/common';
-import {Subscription} from 'rxjs';
+import {distinctUntilChanged, startWith, Subscription} from 'rxjs';
 import {SortService} from '../../service/sort.service';
 import {SortableColumnComponent} from '../../shared/sortable-column/sortable-column.component';
 import {SortableTableDirective} from '../../shared/sortable-column/sortable-table.directive';
 import {AuthService} from '../../auth/auth.service';
+import {filter} from 'rxjs/operators';
 
 @Component({
     selector: 'nmaas-applications',
@@ -25,38 +26,50 @@ export class AppListComponent implements OnInit, OnDestroy {
 
     public appsView: AppViewType;
     public domainId: number;
-
     public selectedDomain: Subscription;
 
     constructor(protected userDataService: UserDataService,
-                private router: Router,
-                private route: ActivatedRoute,
-                private authService: AuthService,
-                private location: Location) {
+                private readonly router: Router,
+                private readonly route: ActivatedRoute,
+                private readonly authService: AuthService,
+                private readonly location: Location) {
     }
 
-    ngOnInit() {
-        const sessionStorageDomainId: number = Number(sessionStorage.getItem('selectedDomainId'))
-        if (this.route.snapshot.data.appViewType !== undefined) {
-            this.appsView = AppViewType.APPLICATION;
-        } else {
-            this.appsView = this.route.snapshot.data.appView;
+    ngOnInit(): void {
+        const sessionStorageDomainId = Number(sessionStorage.getItem('selectedDomainId')) || 0;
+        const defaultDomainId = this.getDefaultDomainId(sessionStorageDomainId);
+        this.appsView = this.route.snapshot.data.appViewType !== undefined
+            ? AppViewType.APPLICATION
+            : this.route.snapshot.data.appView;
+        this.userDataService.selectedDomainId
+            .pipe(
+                filter((domainId): domainId is number =>
+                    domainId !== null
+                    && domainId !== undefined
+                    && domainId !== 0),
+                distinctUntilChanged(),
+                startWith(defaultDomainId)
+            )
+            .subscribe(domainId => {
+                this.domainId = domainId;
+                sessionStorage.setItem('selectedDomainId', String(domainId));
+            });
+    }
+
+    private getDefaultDomainId(sessionStorageDomainId: number): number {
+        if (sessionStorageDomainId !== 0) {
+            return sessionStorageDomainId;
         }
 
-        this.selectedDomain = this.userDataService.selectedDomainId.subscribe(domainId => {
-            if (domainId !== 0) {
-                // if domain id is not equal 0, it is assumed that everything works just fine
-                this.domainId = domainId;
-            } else if (sessionStorageDomainId !== 0) {
-                // session storage contains information about the selected domain
-                this.domainId = sessionStorageDomainId;
-            } else {
-                // otherwise, user domain id's are selected and first one is chosen, usually global domain
-                const domains = this.authService.getDomains();
-                this.domainId = domains.length > 0 ? domains[0] : 0;
-                this.userDataService.selectDomainId(this.domainId);
-            }
-        });
+        const domains = this.authService.getDomains();
+        if (domains.length > 0) {
+
+            const id = domains[0];
+            this.userDataService.selectDomainId(id);
+            return id;
+        }
+
+        return 1;
     }
 
     ngOnDestroy(): void {
