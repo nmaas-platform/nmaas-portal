c4fa6b337252d980b04529a4c0b6eaabbda58f23
jkazmierczak <jkazmierczak@man.poznan.pl>
2025-11-12 11:38:49 +0100

    added label domain in details and changed type for domain

--- src/app/appmarket/admin/webhook/webhook-list/webhook-list.component.ts
+++ src/app/appmarket/admin/webhook/webhook-list/webhook-list.component.ts
@@ -33,14 +33,19 @@ export class WebhookListComponent implements OnInit {
 
   public authRequired: boolean = false;
 
-  public type = [
+  public globalType = [
     { name: "DOMAIN_ACTION", value: "DOMAIN_ACTION" },
     { name: "DOMAIN_GROUP_ACTION", value: "DOMAIN_GROUP_ACTION" },
     { name: "APPLICATION_DEPLOYMENT", value: "APPLICATION_DEPLOYMENT" },
     { name: "APPLICATION_REMOVAL", value: "APPLICATION_REMOVAL"},
     { name: "USER_ASSIGNMENT", value: "USER_ASSIGNMENT" }
-
   ]
+  public domainType = [
+    { name: "APPLICATION_DEPLOYMENT", value: "APPLICATION_DEPLOYMENT" },
+    { name: "APPLICATION_REMOVAL", value: "APPLICATION_REMOVAL"},
+    { name: "USER_ASSIGNMENT", value: "USER_ASSIGNMENT" }
+  ]
+  public type = this.globalType
 
   @ViewChild(ModalComponent, { static: true })
   public modal: ModalComponent;
@@ -74,12 +79,14 @@ export class WebhookListComponent implements OnInit {
     this.pipeRefresh = this.userDataService.selectedDomainId.pipe(debounceTime(300)).subscribe((domainId) => {
       this.selectedDomain = domainId;
       if (domainId !== this.domainGlobalId) {
+        this.type = this.domainType;
         this.service.getByDomain(domainId).subscribe(result => {
           console.log(result);
           this.webhooks = result;
           this.filterWebhooks()
         });
       } else {
+        this.type = this.globalType;
         this.service.getAll().subscribe(result => {
           this.webhooks = result;
           console.log(result);
