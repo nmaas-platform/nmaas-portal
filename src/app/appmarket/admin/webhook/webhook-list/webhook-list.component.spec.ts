092449de86d6be29a53cbe55e89caee8b087e281
pkazimierowski <pkazimierowski@man.poznan.pl>
2026-03-26 10:24:32 +0100

    removed redundant test

--- src/app/appmarket/admin/webhook/webhook-list/webhook-list.component.spec.ts
+++ src/app/appmarket/admin/webhook/webhook-list/webhook-list.component.spec.ts
@@ -86,14 +86,7 @@ describe('WebhookListComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled).toBeTruthy();
   });
-
-  // it('should refresh list and set webkooks', fakeAsync(() => {
-  //   component.refreshList();
-  //   tick(400);
-  //   expect(service.getAll).toHaveBeenCalled();
-  //   fixture.detectChanges();
-  //   expect(component.webhooks.length).toBeGreaterThan(0);
-  // }));
+  
 
   it('should open modal and set default event type', () => {
     component.openModal();
