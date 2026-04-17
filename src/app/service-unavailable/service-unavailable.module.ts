6a2764303e77643d7b4047c7eb32b89d81a0c751
jkazmierczak <jkazmierczak@man.poznan.pl>
2026-04-17 10:51:25 +0200

    dropdown on error page

--- src/app/service-unavailable/service-unavailable.module.ts
+++ src/app/service-unavailable/service-unavailable.module.ts
@@ -3,17 +3,23 @@ import { CommonModule } from '@angular/common';
 import { ServiceUnavailableComponent } from './service-unavailable.component';
 import {RouterModule} from '@angular/router';
 import {TranslateModule} from '@ngx-translate/core';
+import {PrimeTemplate} from 'primeng/api';
+import {SelectModule} from 'primeng/select';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     ServiceUnavailableComponent
   ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    TranslateModule,
-  ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        TranslateModule,
+        PrimeTemplate,
+        SelectModule,
+        FormsModule
+    ],
   exports: [
   ]
 })
