e132de332ae17259779f707afdb0abe5c8b90b95
jkazmierczak <jkazmierczak@man.poznan.pl>
2026-05-20 12:05:15 +0200

    fixed selector

--- src/app/shared/admin/clusters/add-cluster/add-cluster.component.ts
+++ src/app/shared/admin/clusters/add-cluster/add-cluster.component.ts
@@ -76,7 +76,7 @@ export class AddClusterComponent implements OnInit {
         this.stopTranslationUpdate();
       }
     }, 200);
-
+    console.log(this.certificateConfigOption, "WWWWWWWWWWWWWWWWWWWWWWWW")
 
   }
 
